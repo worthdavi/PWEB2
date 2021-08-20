b7399bae1702cf2281261952570fd8111b2fc8ec
davi costa <worthdavi@gmail.com>
2021-08-20 19:45:39 -0300

    Calculadora OK

--- Calculadora/javascript.js
+++ Calculadora/javascript.js
@@ -1,12 +1,80 @@
-var numbers_class = document.getElementsByClassName('buttons').querySelectorAll('numbers')
-var operators_class = document.getElementsByClassName('buttons').querySelectorAll('operators')
 
-function setNumber(value){
-    let screen = document.getElementById('screen')
-    screen.innerText = value
+var buttons = document.querySelectorAll('button')
+var operations = []
+
+function writeOnScreen(x, number){
+    const display = document.querySelector('#display')
+
+    if(!number)
+        operations.push(x)
+    if(x == "=")
+        return
+
+    display.innerText += x
 }
 
-for(let n of numbers_class){
-    let value = n.getAttribute('data-val')
-    n.addEventListener('click', setNumber(value), false)
+var features = [
+    '+',
+    '-',
+    '/',
+    '*',
+    ',',
+    'c'
+]
+
+function executeOperation(operator, value1, value2){
+    let result = 0
+    if(operator == '+'){
+        result = (value1 + value2);
+    }else if (operator == '-') {
+        result = (value1 - value2);
+    }else if (operator == 'x') {
+        result = (value1 * value2);
+    }else if (operator == '÷') {
+        result = (value1 / value2);
+    }
+    return result
 }
+
+function calculate(){
+    const display = document.querySelector('#display')
+    let text = display.innerText
+    for(let i = 0; i < features.length; i++){
+       text = text.replace(\d+,?\d, ' ')
+    }
+    let numeros = text.split(" ")
+    let result = parseInt(numeros[0])
+    let operator = 0
+    for(let i = 0; i < numeros.length; i++){
+        if(parseInt(numeros[i]) && parseInt(numeros[i + 1]) && operations[operator] != null){
+            result = executeOperation(operations[operator], result, parseInt(numeros[i + 1]))
+            operator++
+        }
+    }
+    alert('Resultado: ' + result)
+}
+
+function clear(){
+    const display = document.querySelector('#display')
+    display.innerText = ""
+    operations = []
+}
+
+buttons.forEach(button => {
+    button.addEventListener('click', event => {
+        let value = event.target.id;
+        let isNumber = false
+
+        if(parseInt(value))
+            isNumber = true
+
+        writeOnScreen(value, isNumber)
+
+        if(value == "="){
+            calculate()
+            
+        }else if(value == 'clear'){
+            clear()
+        }
+    });
+ });
